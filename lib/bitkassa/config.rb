ea934d80aaffa90187888fcab35050092750ae77
Bèr Kessels <ber@berk.es>
2015-11-14 12:25:07 +0100

    Allow setting debug modus in config. en/disabling HTTPI logging

--- lib/bitkassa/config.rb
+++ lib/bitkassa/config.rb
@@ -4,8 +4,22 @@ module Bitkassa
                   :merchant_id,
                   :base_uri
 
+    attr_reader   :debug
+
     def initialize
       @base_uri = "https://www.bitkassa.nl/api/v1"
+      self.debug = false
+    end
+
+    def debug=(mode)
+      @debug = mode
+
+      if mode
+        HTTPI.log = true
+        HTTPI.log_level = :debug
+      else
+        HTTPI.log = false
+      end
     end
   end
 end
--- test/lib/config_test.rb
+++ test/lib/config_test.rb
@@ -20,4 +20,20 @@ describe Bitkassa::Config do
       Bitkassa.config.base_uri.must_equal "https://www.bitkassa.nl/api/v1"
     end
   end
+
+  describe "debug" do
+    it "defaults to false" do
+      Bitkassa.config.debug.must_equal false
+    end
+
+    it "is settable" do
+      Bitkassa.config.debug = true
+      Bitkassa.config.debug.must_equal true
+    end
+
+    it "sets httpi log" do
+      Bitkassa.config.debug = false
+      HTTPI.log?.must_equal false
+    end
+  end
 end
