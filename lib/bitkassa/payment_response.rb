bed7f3fbdb5302b784603dba920205cb2f6ec2b0
Bèr Kessels <ber@berk.es>
2015-11-14 15:53:01 +0100

    Allow payment response to get all attributes that will be returned.

--- lib/bitkassa/payment_response.rb
+++ lib/bitkassa/payment_response.rb
@@ -1,15 +1,26 @@
 module Bitkassa
+  # A +PaymentResponse+ represents the immediate response after requesting a
+  # payment. It can will parse the JSON and contain the information nessecary
+  # to determine the status of the payment request.
   class PaymentResponse
-    attr_accessor :payment_id, :payment_status, :meta_info
+    attr_accessor :payment_id,
+                  :payment_url,
+                  :address,
+                  :amount,
+                  :bitcoin_url,
+                  :expire,
+                  :error,
+                  :success
 
     def self.from_json(json)
       new(JSON.parse(json))
     end
 
     def initialize(attributes)
-      @payment_id     = attributes["payment_id"]
-      @payment_status = attributes["payment_status"]
-      @meta_info      = attributes["meta_info"]
+      attributes.each do |key, value|
+        setter_method = "#{key.to_s}=".to_sym
+        self.send(setter_method, value)
+      end
     end
   end
 end
