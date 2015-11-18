b389c431703c5367cf740c5753f9fd1815f2583e
Bèr Kessels <ber@berk.es>
2015-11-18 18:52:22 +0100

    Replace magic glob-require with declarative require.
    This avoids load dependency issues, where one file has to be required
    before another one.

--- lib/bitkassa.rb
+++ lib/bitkassa.rb
@@ -2,6 +2,15 @@ require "json"
 require "base64"
 require "httpi"
 
+require_relative "bitkassa/authentication"
+require_relative "bitkassa/request"
+require_relative "bitkassa/config"
+require_relative "bitkassa/exception"
+require_relative "bitkassa/payment_request"
+require_relative "bitkassa/status_request"
+require_relative "bitkassa/payment_response"
+require_relative "bitkassa/payment_result"
+
 ##
 # Bitkassa Namespace
 module Bitkassa
@@ -13,5 +22,3 @@ module Bitkassa
     @config ||= Bitkassa::Config.new
   end
 end
-
-Dir["./lib/bitkassa/*.rb"].each { |file| require file }
