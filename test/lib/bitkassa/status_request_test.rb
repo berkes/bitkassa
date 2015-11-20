9d2e4374e4579ad64e12ee30714c4e15574b4b45
Bèr Kessels <ber@berk.es>
2015-11-20 18:54:26 +0100

    Adding status-request.

---
+++ test/lib/bitkassa/status_request_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+describe Bitkassa::StatusRequest do
+  before do
+    Bitkassa.config.secret_api_key = "SECRET"
+    Bitkassa.config.merchant_id = "banketbakkerhenk"
+  end
+
+  describe "#attributes" do
+    it "has payment_id" do
+      attributes = {
+        payment_id: "dhqe4cnj7f"
+      }
+
+      bitkassa = Bitkassa::StatusRequest.new(attributes)
+      bitkassa.attributes.must_equal attributes
+    end
+  end
+
+  describe "#payload_action" do
+    it { subject.payload_action.must_equal "get_payment_status" }
+  end
+
+  describe "#can_perform?" do
+    describe "when payment_id is empty" do
+      before { subject.payment_id = nil }
+      it { subject.can_perform?.must_equal false }
+    end
+  end
+
+  private
+
+  def subject
+    @subject ||= Bitkassa::StatusRequest.new(payment_id: "dhqe4cnj7f")
+  end
+end
